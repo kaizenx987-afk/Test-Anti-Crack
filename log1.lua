69f1b622dd5cfc33fb77f42067140a0ebe8b1fe1
kaizenx987-afk <kaizenx987@gmail.com>
2026-08-26 15:09:42 +0800

    Update log1.lua

--- log1.lua
+++ log1.lua
@@ -968,17 +968,7 @@ function autoBypass()
     HexPatches.MemoryPatch("libanogs.so", 0x497E64, "h00 00 80 D2 C0 03 5F D6", 32)
     HexPatches.MemoryPatch("libanogs.so", 0x1FF3A4, "h00 00 80 D2 C0 03 5F D6", 32)
 
-    HexPatches.MemoryPatch("libanogs.so", 0x11C5A4, "h00 00 80 D2 C0 03 5F D6", 32);
-    HexPatches.MemoryPatch("libanogs.so", 0x1CEB14, "hC0 03 5F D6", 32);
-    HexPatches.MemoryPatch("libanogs.so", 0x1CF808, "h00 00 80 D2 C0 03 5F D6", 32);
-    HexPatches.MemoryPatch("libanogs.so", 0x1D3E98, "hC0 03 5F D6", 32);
-    HexPatches.MemoryPatch("libanogs.so", 0x238DAC, "hC0 03 5F D6", 32);
-    HexPatches.MemoryPatch("libanogs.so", 0x264688, "hC0 03 5F D6", 32);
-    HexPatches.MemoryPatch("libanogs.so", 0x2649A4, "hC0 03 5F D6", 32);
-    HexPatches.MemoryPatch("libanogs.so", 0x2652C8, "hC0 03 5F D6", 32);
-    HexPatches.MemoryPatch("libanogs.so", 0x265A40, "hC0 03 5F D6", 32);
-    HexPatches.MemoryPatch("libanogs.so", 0x2A55D4, "hC0 03 5F D6", 32);
-    HexPatches.MemoryPatch("libanogs.so", 0x2A5634, "hC0 03 5F D6", 32);
+    
     HexPatches.MemoryPatch("libanogs.so", 0x2DAB74, "hC0 03 5F D6", 32);
   end)
   
